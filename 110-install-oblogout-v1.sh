6b6c59fcc6effd534502cf7988ac499bff27184c
BogdanZsolt <bogdan.zsoltika@gmail.com>
2019-05-27 09:28:52 +0200

    change directory

--- 110-install-oblogout-v1.sh
+++ 110-install-oblogout-v1.sh
@@ -14,7 +14,7 @@ echo "###############################"
 # dependencies
 sudo apt install -y python-gtk2-dev
 sudo apt install -y python-distutils-extra
-sudo apt install -y python-imaging
+#sudo apt install -y python-imaging
 sudo apt install -y python-pil
 
 rm -rf ~/temp/oblogout
@@ -25,7 +25,7 @@ sudo rm -rf ~/temp/oblogout
 
 rm -rf ~/temp/oblogout-themes
 git clone https://github.com/arcolinux/arcolinux-oblogout-themes.git ~/temp/oblogout-themes
-sudo mv ~/temp/oblogout-themes
+sudo cp -rf ~/temp/oblogout-themes/usr /
 rm -rf ~/temp/oblogout-themes
 
 echo
